63e2f850fada3fa8d54b17b10021cce40912dcca
JunkyDeveloper <junkydeveloper@gmail.com>
2025-11-16 17:37:11 +0100

    moved indexing into flint-core

--- flint-steel/src/main.rs
+++ flint-steel/src/main.rs
@@ -1,7 +1,6 @@
-mod io;
-
-use crate::io::{generate_index, get_all_tests_paths, load_scoped_tests};
 use std::env;
+use std::path::Path;
+use flint_core::test_spec::TestSpec;
 
 const TEST_PATH: &str = "./test";
 fn main() {
@@ -11,13 +10,13 @@ fn main() {
         match args[1].as_str() {
             "index" => {
                 println!("index");
-                if let Err(err) = generate_index(TEST_PATH) {
+                if let Err(err) = TestSpec::generate_index(&Path::new(TEST_PATH)) {
                     println!("error while generating index: {}", err);
                 }
             }
             _ => {
                 println!("Will run tests on a specific scope");
-                match load_scoped_tests(&args[1..]) {
+                match TestSpec::load_tagged_tests_paths(&args[1..]) {
                     Ok(_test_paths) => {
                         test_paths = _test_paths;
                     }
@@ -30,7 +29,7 @@ fn main() {
     } else {
         // Loads all test from the index
         println!("Will run all tests");
-        match get_all_tests_paths() {
+        match TestSpec::get_all_tests_paths() {
             Ok(_test_paths) => test_paths = _test_paths,
             Err(err) => {
                 println!("error while getting all_tests_paths: {}", err);
